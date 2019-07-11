9e3eed06e61a7417557472714227cc454738bfd4
Steve Journeay <sjournea@taralaur.com>
2019-07-10 17:51:25 -0700

    Moved tests to tests/ and code to lib/

---
+++ tests/test_calculator.py
@@ -0,0 +1,11 @@
+"""test_calculator.py"""
+import lib.calculator as calc
+
+
+class TestCalculator:
+    def test_addition(self):
+        assert 4 == calc.add(2, 2)
+
+    def test_subtraction(self):
+        assert 2 == calc.subtract(4, 2)
+
